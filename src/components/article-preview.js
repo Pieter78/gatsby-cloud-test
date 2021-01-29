480dc06383c09f6e21a6177a4b434bf01c8b7ead
Pieter Overbeek <pieter@belfabriek.nl>
2021-01-29 14:11:38 +0200

    add test

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react";
+import { Link } from "gatsby";
+import Img from "gatsby-image";
 
-import styles from './article-preview.module.css'
+import styles from "./article-preview.module.css";
 
 export default ({ article }) => (
   <div className={styles.preview}>
@@ -10,6 +10,7 @@ export default ({ article }) => (
     <h3 className={styles.previewTitle}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
+    <h2>EVEN TESTEN</h2>
     <small>{article.publishDate}</small>
     <p
       dangerouslySetInnerHTML={{
@@ -17,4 +18,4 @@ export default ({ article }) => (
       }}
     />
   </div>
-)
+);
